1391baf630c72e73c459458c1a6855f3d4f660bd
Tisha <tisha_h20@yahoo.com>
2022-09-13 01:59:12 -0400

    api endpoint updates

--- src/redux_store/actions/expenseActions.js
+++ src/redux_store/actions/expenseActions.js
@@ -19,7 +19,8 @@ export const GET_ALL_USER_EXPENSES_FAILURE = 'GET_ALL_USER_EXPENSES_FAILURE';
 export const addNewExpense = newExpenseInfo => dispatch => {
 
     dispatch({ type: ADD_EXPENSE_START });
-    axiosWithAuth().post('http://localhost:5000/api/bills/', newExpenseInfo)
+    //axiosWithAuth().post('http://localhost:5000/api/bills/', newExpenseInfo)
+    axiosWithAuth().post('https://split-the-bill-app-main.herokuapp.com/api/bills/', newExpenseInfo)
     .then(res => { 
         console.log("add expense data returned from server in actions", res.data)            
         dispatch({ type: ADD_EXPENSE_SUCCESS, payload: res.data })        
@@ -32,7 +33,8 @@ export const addNewExpense = newExpenseInfo => dispatch => {
 
 export const getExpenseToEdit = (expenseId) => dispatch => {
     dispatch({ type: GET_EXPENSE_TO_EDIT_START })
-    axiosWithAuth().get(`http://localhost:5000/api/bills/${expenseId}`)
+    //axiosWithAuth().get(`http://localhost:5000/api/bills/${expenseId}`)
+    axiosWithAuth().get(`https://split-the-bill-app-main.herokuapp.com/api/bills/${expenseId}`)
     .then(res => {
         console.log("get expense to edit in actions", res.data);
         dispatch({ type: GET_EXPENSE_TO_EDIT_SUCCESS, payload: res.data })
@@ -45,7 +47,8 @@ export const getExpenseToEdit = (expenseId) => dispatch => {
 
 export const editExistingExpense = (editExpenseInfo, expenseId) => dispatch => {
     dispatch({ type: EDIT_EXPENSE_START})
-    axiosWithAuth().put(`http://localhost:5000/api/bills/${expenseId}`, editExpenseInfo)
+    //axiosWithAuth().put(`http://localhost:5000/api/bills/${expenseId}`, editExpenseInfo)
+    axiosWithAuth().put(`https://split-the-bill-app-main.herokuapp.com/api/bills/${expenseId}`, editExpenseInfo)
     .then(res => {
         console.log("edit expense data in actions", res.data)
         dispatch({ type: EDIT_EXPENSE_SUCCESS, payload: res.data})
@@ -58,7 +61,8 @@ export const editExistingExpense = (editExpenseInfo, expenseId) => dispatch => {
 
 export const deleteExpense = (expenseId) => dispatch => {
     dispatch({ type: DELETE_EXPENSE_START })
-    axiosWithAuth().delete(`http://localhost:5000/api/bills/${expenseId}`)
+    //axiosWithAuth().delete(`http://localhost:5000/api/bills/${expenseId}`)
+    axiosWithAuth().delete(`https://split-the-bill-app-main.herokuapp.com/api/bills/${expenseId}`)
     .then(res => {
         console.log("delete expense data in actions", res.data)
         dispatch({ type: DELETE_EXPENSE_SUCCESS, payload: res.data})
@@ -71,7 +75,8 @@ export const deleteExpense = (expenseId) => dispatch => {
 
 export const getAllExpenses = userId => dispatch => {
     dispatch({ type: GET_ALL_USER_EXPENSES_START })
-    axiosWithAuth().get(`http://localhost:5000/api/users/${userId}/bills`)
+    //axiosWithAuth().get(`http://localhost:5000/api/users/${userId}/bills`)
+    axiosWithAuth().get(`https://split-the-bill-app-main.herokuapp.com/api/users/${userId}/bills`)
     .then(res => {
         console.log("get all user expenses data in actions", res.data)
         dispatch({ type: GET_ALL_USER_EXPENSES_SUCCESS, payload: res.data })
--- src/redux_store/actions/userActions.js
+++ src/redux_store/actions/userActions.js
@@ -14,7 +14,8 @@ export const GET_USER_DETAILS_FAILURE = 'GET_USER_DETAILS_FAILURE';
 
 export const registerUser = newUserInfo => dispatch => {
     dispatch({ type: REGISTER_USER_START });
-    axios.post('http://localhost:5000/api/users/register', newUserInfo)
+    //axios.post('http://localhost:5000/api/users/register', newUserInfo)
+    axios.post('https://split-the-bill-app-main.herokuapp.com/api/users/register', newUserInfo)
     .then(res => { 
         console.log("register data in actions", res)            
         dispatch({ type: REGISTER_USER_SUCCESS, payload: res.data })        
@@ -34,7 +35,8 @@ export const registerUser = newUserInfo => dispatch => {
     //automatically logs the user in after registration
     .then(() => {                             
         dispatch ({ type: LOGIN_USER_START });
-        axios.post('http://localhost:5000/api/users/login', {email: newUserInfo.email, password: newUserInfo.password})
+        //axios.post('http://localhost:5000/api/users/login', {email: newUserInfo.email, password: newUserInfo.password})
+        axios.post('https://split-the-bill-app-main.herokuapp.com/api/users/login', {email: newUserInfo.email, password: newUserInfo.password})
         .then(res => {            
             console.log("automatic login after register data in front end", res.data)
             dispatch({ type: LOGIN_USER_SUCCESS, payload: res.data})            
@@ -58,7 +60,8 @@ export const registerUser = newUserInfo => dispatch => {
 export const loginUser = loginCredentials => dispatch => {
 
     dispatch({ type: LOGIN_USER_START });
-    axios.post('http://localhost:5000/api/users/login', loginCredentials)
+    //axios.post('http://localhost:5000/api/users/login', loginCredentials)
+    axios.post('https://split-the-bill-app-main.herokuapp.com/api/users/login', loginCredentials)
     .then(res => {        
         console.log("login data in actions", res.data) 
         dispatch({ type: LOGIN_USER_SUCCESS, payload: res.data })         
@@ -83,7 +86,8 @@ export const logoutUser = () => dispatch => {
 export const getUserDetails = userId => dispatch => {
     dispatch({ type: GET_USER_DETAILS_START });
 
-    axiosWithAuth().get(`http://localhost:5000/api/users/${userId}`)
+    //axiosWithAuth().get(`http://localhost:5000/api/users/${userId}`)
+    axiosWithAuth().get(`https://split-the-bill-app-main.herokuapp.com/api/users/${userId}`)
     .then(res => {
         console.log("user details in actions", res.data);
         dispatch({ type: GET_USER_DETAILS_SUCCESS, payload: res.data });
